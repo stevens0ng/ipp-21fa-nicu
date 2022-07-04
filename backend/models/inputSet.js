5c372fd9896cc326ac2d8cc32d73a18f5be5fc84
Steven Song <89991942+stevens0ng@users.noreply.github.com>
2022-07-04 11:43:10 -0400

    Add files via upload

---
+++ backend/models/inputSet.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const inputSet = new mongoose.Schema({
+  gestational_age: {
+    type: Number,
+    required: true,
+  },
+  postnatal_age: {
+    type: Number,
+    required: true,
+  },
+  birth_weight: {
+    type: Number,
+    required: true,
+  },
+  current_weight: {
+    type: Number,
+    required: true,
+  },
+  time_sent: {
+    type: String,
+    required: true,
+  },
+  pathogen_isolated: {
+    type: String,
+    required: true,
+  },
+  site_of_infection: {
+    type: String,
+    required: true,
+  },
+  blood_dropdown_selection: {
+    type: String,
+    required: false,
+  },
+  abdominal_involvement: {
+    type: String,
+    required: true,
+  },
+  output_available: {
+    type: Boolean,
+    required: true
+  }
+  
+});
+module.exports = mongoose.model("inputSet", inputSet);
